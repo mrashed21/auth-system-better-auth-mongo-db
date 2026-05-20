5cc524545b80135803a41710e408f88b7eadfeb7
mrashed21 <rashedjaman768@gmail.com>
2026-05-20 16:17:29 +0600

    feat: remove unused route for fetching all users

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -7,6 +7,5 @@ router.route("/register").post(auth_controller.register);
 router.route("/verify-otp").post(auth_controller.verify_otp);
 router.route("/resend-otp").post(auth_controller.resend_otp);
 router.route("/login").post(auth_controller.login);
-router.route("/all-users").get(auth_controller.get_all_users);
 
 export const auth_router = router;
